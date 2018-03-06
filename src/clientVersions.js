65f7d3fa16e9fb6ea6ac757ea2faa610c107297e
F0RIS <f0risw@gmail.com>
2018-03-06 11:00:31 +0200

    Updated client versions

--- src/clientVersions.js
+++ src/clientVersions.js
@@ -3,10 +3,10 @@ var AppInfo = require('./models/appinfo.js');
 
 //clients versions
 var clientsVersions = {
-    amazon: new AppInfo(137, "5.9.0", ""),
-    gp: new AppInfo(137, "5.9.0", ""),
+    amazon: new AppInfo(148, "7.0.2", ""),
+    gp: new AppInfo(148, "7.0.2", ""),
     testVersion: new AppInfo(89, "", ""),
-    ios: new AppInfo("1.1.9", "", ""),
+    ios: new AppInfo("1.2.5", "", ""),
 };
 
 module.exports = clientsVersions;
